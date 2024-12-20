861c74ba6fc495bbc78f087d069e7c5a0222e080
Madhav P <madhavp2023@gmail.com>
2024-12-20 19:11:25 +0530

    Learning Phase-3 Completed

---
+++ MyEnvironment/Arrays.py
@@ -0,0 +1,37 @@
+# An array is an object that stores a group of elements (or values) of same data type. The main advantage of array is to store and process a group of elements easily.
+
+# Key Points :- 1) Arrays can store only one type of data.
+#               2) Arrays can increase or decrease their size dynamically.
+
+
+# Advantages of Arrays :- 
+''' 1) Arrays are similar to lists. The main difference is that arrays can store only one type
+of elements; whereas, lists can store different types of elements. When dealing with a
+huge number of elements, arrays use less memory than lists and they offer faster
+execution than lists.'''
+
+''' 2) The size of the array is not fixed in Python. Hence, we need not specify how many
+elements we are going to store into an array in the beginning.'''
+
+
+''' 3) Arrays can grow or shrink in memory dynamically (during runtime).'''
+
+
+''' 4) Arrays are useful to handle a collection of elements like a group of numbers or
+characters.'''
+
+''' 5) Methods that are useful to process the elements of any array are available in 'array'
+module '''
+
+# Creating an Array :- Arrays can hold data of same type. The type should be specified by using a type code at the time of creating the array object as: - 
+
+# Syntax :- arrayname = array(type code, [elements])
+
+# Example :- a = array('i', [4, 6, 2, 9])
+
+'''
+The type code 'i', represents integer type array where we can store integer numbers. If
+the type code is 'f' then it represents float type array where we can store numbers with
+decimal point.
+
+'''
