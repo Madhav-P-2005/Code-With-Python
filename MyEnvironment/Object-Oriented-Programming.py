f34390abebe65e8f97b865f12d5463e8539f2074
Madhav P <madhavp2023@gmail.com>
2024-12-29 23:46:39 +0530

    Learning Phase-9 Completed

---
+++ MyEnvironment/Object-Oriented-Programming.py
@@ -0,0 +1,25 @@
+# Problems in Procedure Oriented Approach :-
+
+'''
+
+1) Procedural programming mainly focuses on procedures or functions. Less attention is
+given to the data.
+
+2) The data and functions are separate from each other.
+
+3) Global data is freely moving and is shared among various functions. Thus, it becomes
+difficult for programmers to identify and fix issues in a program that originate due to
+incorrect data handling.
+
+4) Changes in data types need to be carried out manually all over the program and in the
+functions using the same data type.
+
+5) Limited and difficult code reusability.
+
+6) It does not model real-world entities (e.g., car, table, bank account, loan) very well where
+we as a human being, perceive everything as an object.
+
+7) The procedural programming approach does not work well for large and complex
+systems.
+
+'''
